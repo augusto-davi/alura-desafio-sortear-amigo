5da4f26d4f49301d0cd8fab690a902362d5cd3b9
Davi <obrigatoriopcional@gmail.com>
2025-02-14 19:44:54 -0300

    add function adicionarAmigo()

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 let amigos = [];
+
+function adicionarAmigo(){
+  const inputAmigo = document.querySelector("#amigo");
+  const amigo = inputAmigo.value;  
+  if(!(amigo.match("[a-zA-Z]"))){
+    alert("Por favor, insira um nome.");
+  }
+  else{
+    amigos.push(amigo);
+  }
+  inputAmigo.value = "";
+  inputAmigo.focus();
+}
